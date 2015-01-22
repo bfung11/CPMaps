80214bd7dcc1c241d1162830f46626048fdec64f
bfung11 <bfung11@gmail.com>
2015-01-21 18:52:39 -0800

    Initial Commit

---
+++ CP_MapsTests/CP_MapsTests.swift
@@ -0,0 +1,36 @@
+//
+//  CP_MapsTests.swift
+//  CP_MapsTests
+//
+//  Created by Brian Fung on 1/21/15.
+//  Copyright (c) 2015 Carl-Brian. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CP_MapsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
