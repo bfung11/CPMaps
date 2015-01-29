b0a0201b47d52adc1a74780c4e0e26ba1dc4fa8b
bfung11 <bfung11@gmail.com>
2015-01-28 19:09:25 -0800

    Added a new file

---
+++ CP_Maps/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  CP_Maps
+//
+//  Created by Brian Fung on 1/28/15.
+//  Copyright (c) 2015 Carl-Brian. All rights reserved.
+//
+
+import Foundation
+
+class ViewController: UITableViewController {
+}
