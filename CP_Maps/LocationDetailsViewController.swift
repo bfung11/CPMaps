1c88f40a7b6a4b93716ff2af71c13141d2fc341e
bfung11 <bfung11@gmail.com>
2015-02-11 22:27:42 -0800

    - Added methods for exit responder

---
+++ CP_Maps/LocationDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LocationDetailsViewController.swift
+//  CP_Maps
+//
+//  Created by Brian Fung on 2/11/15.
+//  Copyright (c) 2015 Carl-Brian. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBAction func cancelToPlayersViewController(segue:UIStoryboardSegue) {
+
+    }
+
+    @IBAction func savePlayerDetail(segue:UIStoryboardSegue) {
+
+    }
+}
