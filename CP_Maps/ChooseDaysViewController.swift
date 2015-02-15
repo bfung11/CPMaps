fe990702de27cdaae8e4168ee8adb267bf08db31
bfung11 <bfung11@gmail.com>
2015-02-14 18:25:40 -0800

    - Adding new table view controller to Choose Days selector - Adding ability to choose a day

---
+++ CP_Maps/ChooseDaysViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ChooseDaysViewController.swift
+//  CP_Maps
+//
+//  Created by Brian Fung on 2/14/15.
+//  Copyright (c) 2015 Carl-Brian. All rights reserved.
+//
+
+import UIKit
+
+class ChooseDaysViewController: UITableViewController {
+   var days: [String]!
+   var selectedDays: String? = nil
+   var selectedDaysIndex: Int? = nil
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      days = ["Monday", "Tuesday", "Wednesday",
+         "Thursday", "Friday", "Saturday", "Sunday"]
+   }
+   
+   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+      return 1
+   }
+   
+   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return days.count
+   }
+   
+   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCellWithIdentifier("DaysCell", forIndexPath: indexPath) as UITableViewCell
+      cell.textLabel?.text = days[indexPath.row]
+      
+      if indexPath.row == selectedDaysIndex {
+         cell.accessoryType = .Checkmark
+      } else {
+         cell.accessoryType = .None
+      }
+      return cell
+   }
+   
+   //MARK: - Table view delegate
+   
+   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+      tableView.deselectRowAtIndexPath(indexPath, animated: true)
+      
+      //Other row is selected - need to deselect it
+      if let index = selectedDaysIndex {
+         let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: index, inSection: 0))
+         cell?.accessoryType = .None
+      }
+      
+      selectedDaysIndex = indexPath.row
+      selectedDays = days[indexPath.row]
+      
+      //update the checkmark for the current row
+      let cell = tableView.cellForRowAtIndexPath(indexPath)
+      cell?.accessoryType = .Checkmark
+   }
+}
