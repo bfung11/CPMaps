8dfa7dd417a3073b26b7de2a1c005654001bc4d8
bfung11 <bfung11@gmail.com>
2015-02-21 22:23:59 -0800

    - Cleaning up code in Location class

--- CP_Maps/Location.swift
+++ CP_Maps/Location.swift
@@ -12,34 +12,12 @@ class Location: NSObject {
    var building : Building
    var course : Course
    
-   /*
-   var buildingName : String
-   var buildingNumber : String
-   var roomNumber : String
-   var className : String
-   var classDaysList : [Day]
-   var classDays : String
-   var classTimes : String
-   */
-   
    init(buildingName: String, buildingNumber: String, roomNumber: String,
       courseTitle: String, daysAsString: [String], startTime: String, endTime: String){
          
          self.building = Building(name: buildingName, number: buildingNumber, room: roomNumber)
          self.course = Course(name: courseTitle, daysAsString: daysAsString,
             startTime: startTime, endTime: endTime)
-         /*
-         self.buildingName = buildingName
-         self.buildingNumber = buildingNumber
-         self.roomNumber = roomNumber
-         self.className = className
-         self.classDaysList = [Day]()
-         self.classDays = ""
-         self.classTimes = classTimes
-         super.init()
-         classDaysList = (convertStringsToDays(classDaysArray))
-         classDaysList.sort({$0.value < $1.value})
-         */
    }
    
    //Optimization: Already created an object before this -> perhaps don't need to recreate it
