2bbee352e979133e321afee744ae984063456c73
bfung11 <bfung11@gmail.com>
2015-04-22 20:01:26 -0700

    Adding import CoreData

--- CP_Maps/Building.swift
+++ CP_Maps/Building.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class Building: NSObject {
    var name: String!
--- CP_Maps/Room.swift
+++ CP_Maps/Room.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class Room: NSObject {
    var number: String!
