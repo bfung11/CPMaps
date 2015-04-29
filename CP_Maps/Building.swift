e3a39c3f8fe3ae8284154f2fd95d034a77a0b0a3
carllindiii <carl.lind.iii@gmail.com>
2015-04-28 22:02:38 -0700

    adding lat, long to building

--- CP_Maps/Building.swift
+++ CP_Maps/Building.swift
@@ -13,11 +13,22 @@ class Building: NSObject {
    var name: String!
    var number: String!
    var rooms: [Room]!
+   var lat : Double!
+   var long: Double!
    
    init(name: String, number: String, rooms: [Room]) {
       self.name = name
       self.number = number
       self.rooms = rooms
+      super.init()
+   }
+   
+   init(name: String, number: String, lat: Double, long: Double) {
+      self.name = name
+      self.number = number
+      self.rooms = []
+      self.lat = lat
+      self.long = long
       super.init()
    }
    
