524b1109462b1a20ef91ecd45fdef9f2df9a2103
Brian Fung <bfung11@gmail.com>
2015-05-02 13:29:36 -0700

    Reorganizing all constants so the organization make sense Adding comments as titles to organization

--- CP_Maps/Const.swift
+++ CP_Maps/Const.swift
@@ -12,30 +12,37 @@ let dayEnumStartValue = -1
 let selectedDaysAsBoolInitialCount = 7
 let selectedDaysAsBoolIntialValue = false
 
+// Cell Reuse Identifiers
 let locationCellReuseIdentifier = "LocationCell"
-let chooseLocationSegueIdentifier = "chooseLocation"
 
+// View Controller Titles
 let editLocationViewControllerTitle = "Edit Location"
 let addLocationViewControllerTitle = "Add Location"
+let chooseBuildingViewControllerTitle = "Building"
+let chooseRoomViewControllerTitle = "Room"
 
+// Number of Sections in view controllers
 let numberOfSectionsInLocationsViewController = 1
 let numberOfSectionsInChooseBuildingRoomViewController = 1
 let numberOfSectionsInDaysViewController = 1
 
-let saveNewLocationTitle = "No Building Selected"
-let saveNewLocationMessage = "Please select a building to create a new location"
+// Segue Identifiers
+let chooseLocationSegueIdentifier = "chooseLocation"
 let editLocationSegueIdentifier = "editLocation"
 let saveLocationSegueIdentifer = "saveLocation"
-let numCharactersToRemoveForFinalLengthOfSelectedDaysString = 2
-
-let chooseBuildingViewControllerTitle = "Building"
 let chooseBuildingSegueIdentifier = "chooseBuilding"
 let saveBuildingSegueIdentifer = "saveBuilding"
-let chooseRoomViewControllerTitle = "Room"
 let chooseRoomSegueIdentifier = "chooseRoom"
 let saveRoomSegueIdentifer = "saveRoom"
+let findBuildingSegueIdentifer = "findBuilding"
 let chooseDaysSegueIdentifier = "chooseDays"
 
-let findBuildingSegueIdentifer = "findBuilding"
+// UIAlertView Messages
+let saveNewLocationTitle = "No Building Selected"
+let saveNewLocationMessage = "Please select a building to create a new location"
 
+// UIAlertView "Cancel" button titles
 let cancelButtonTitleOK = "OK"
+
+let numCharactersToRemoveForFinalLengthOfSelectedDaysString = 2
+
