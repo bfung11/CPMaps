54409a77db7ed6ca50c490e5ead340d37c5af5e2
Brian Fung <bfung11@gmail.com>
2015-05-03 01:00:49 -0700

    Moving loadDataFromCSV to BuildingsPersistencyManager

--- CP_Maps/BuildingsPersistencyManager.swift
+++ CP_Maps/BuildingsPersistencyManager.swift
@@ -16,6 +16,33 @@ class BuildingsPersistencyManager: NSObject {
 //      buildings = buildingsData as [Building]
    }
    
+   func loadDataFromCSV() {
+      var buildingsDataParsed = [Building]()
+      // load in buildings from CSV file -- this only works for Carl at the moment...
+      if let aStreamReader = StreamReader(path: "/Users/carllindiii/Desktop/Brians CPMaps/CP_Maps/Building_Info.csv") {
+         while let line = aStreamReader.nextLine() {
+            var buildingArr = split(line) {$0 == ","}
+            
+            if buildingArr.count == 5 {
+               buildingsDataParsed.append(Building(
+                  number: buildingArr[0],
+                  name: buildingArr[1],
+                  numberOfFloors: (buildingArr[2] as NSString).integerValue,
+                  latitude: (buildingArr[3] as NSString).doubleValue,
+                  longitude: (buildingArr[4] as NSString).doubleValue))
+            }
+            
+            if buildingArr.count == 2 {
+               buildingsDataParsed.append(Building(
+                  number: buildingArr[0],
+                  name: buildingArr[1]))
+            }
+         }
+         aStreamReader.close()
+         buildings = buildingsDataParsed
+      }
+   }
+   
    func setBuildings(newBuildings: [Building]) {
       buildings = newBuildings
    }
--- CP_Maps/CPMapsLibraryAPI.swift
+++ CP_Maps/CPMapsLibraryAPI.swift
@@ -26,31 +26,7 @@ class CPMapsLibraryAPI: NSObject {
    }
    
    func loadDataFromCSV() {
-      var buildingsDataParsed = [Building]()
-      // load in buildings from CSV file -- this only works for Carl at the moment...
-      if let aStreamReader = StreamReader(path: "/Users/carllindiii/Desktop/Brians CPMaps/CP_Maps/Building_Info.csv") {
-         while let line = aStreamReader.nextLine() {
-            var buildingArr = split(line) {$0 == ","}
-            
-            if buildingArr.count == 5 {
-               buildingsDataParsed.append(Building(
-                  number: buildingArr[0],
-                  name: buildingArr[1],
-                  numberOfFloors: (buildingArr[2] as NSString).integerValue,
-                  latitude: (buildingArr[3] as NSString).doubleValue,
-                  longitude: (buildingArr[4] as NSString).doubleValue))
-            }
-         
-            if buildingArr.count == 2 {
-               buildingsDataParsed.append(Building(
-                  number: buildingArr[0],
-                  name: buildingArr[1]))
-            }
-         }
-         aStreamReader.close()
-         // set buildings data
-         buildingsPersistencyManager.setBuildings(buildingsDataParsed)
-      }
+      buildingsPersistencyManager.loadDataFromCSV()
    }
    
    func addLocation(name: String?, buildingNumber: String!, roomNumber: String?, startTime: String?, endTime: String?, days: String?) {
